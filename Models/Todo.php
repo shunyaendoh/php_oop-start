766b6d5561b359451eabbf1926292fa24d3d6b22
shunya_endoh <shunya.endoh@gmail.com>
2019-09-11 08:33:38 +0800

    編集機能まで

---
+++ Models/Todo.php
@@ -0,0 +1,46 @@
+<?php
+
+require_once('config/dbconnect.php');
+class Todo
+{
+    private $table = 'tasks';
+    private $db_manager;
+
+    public function __construct()
+    {
+        $this->db_manager = new DbManager();
+        $this->db_manager->connect();
+    }
+
+    public function create($task)
+    {
+        $stmt = $this->db_manager->dbh->prepare('INSERT INTO ' . $this->table . '(name) VALUES (?)');
+        $stmt->execute([$task]);        
+    }
+    public function getAll()
+    {
+        $stmt = $this->db_manager->dbh->prepare('SELECT * FROM ' . $this->table);
+        $stmt->execute();
+        $results = $stmt->fetchAll();
+        return $results;
+    }
+    public function delete($id)
+    {
+        $stmt = $this->db_manager->dbh->prepare('DELETE FROM ' . $this->table . ' WHERE id = ?');
+        $stmt->execute([$id]);
+    }
+    public function get($num)
+    {
+        $stmt = $this->db_manager->dbh->prepare('SELECT * FROM ' . $this->table . ' WHERE id = ?');
+        $stmt->execute([$num]);
+        $result = $stmt->fetch();
+
+        return $result;
+    }
+    public function update($task, $id)
+    {
+
+        $stmt = $this->db_manager->dbh->prepare('UPDATE ' . $this->table . ' SET name = ? WHERE id = ?');
+        $stmt->execute([$task, $id]);
+    }
+}
